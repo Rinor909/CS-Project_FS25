8391f2211c7f9538af9f2994dab206555d5f3f63
Rinor909 <rinor.abazi@student.unisg.ch>
2025-03-14 15:28:17 +0100

    dasdsa

--- testapp.py
+++ testapp.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.write("Test test les gars")
 st.write("ANNA MADE A CHANGE")
 
-from stremlit_demo.py import lafonction
-print(lafonction(6))
+from streamlit_demo.py import lafonction
+print(lafonction(7))
