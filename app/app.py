9c2019f9c109442921646d0395547c8424be2b23
Rinor909 <rinor.abazi@student.unisg.ch>
2025-05-10 15:29:37 +0200

    gg

--- app/app.py
+++ app/app.py
@@ -63,7 +63,7 @@ def main():
     
     with header_col1:
         # Logo area
-        st.image("https://i.ibb.co/Fb2X2QRB/Logo-Immo-Insight-ZH-w-bg.png", width=100)
+        st.image("https://i.ibb.co/Fb2X2QRB/Logo-Immo-Insight-ZH-w-bg.png", width=200)
         st.caption("VALUESTATE ZÜRICH")
     
     with header_col2:
