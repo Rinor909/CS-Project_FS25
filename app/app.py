c7b5ee5e5398c4426b61a5a4083c9067b36fdf59
Rinor909 <rinor.abazi@student.unisg.ch>
2025-05-10 15:38:36 +0200

    gg

--- app/app.py
+++ app/app.py
@@ -64,7 +64,6 @@ def main():
     with header_col1:
         # Logo area
         st.image("https://i.ibb.co/Fb2X2QRB/Logo-Immo-Insight-ZH-w-bg.png", width=200)
-        st.caption("Immo Insight Zürich")
     
     with header_col2:
         # Title and subtitle
