688e452bcd4a8fbe9ff5a2cad3436a8c0c31504c
Rinor909 <rinor.abazi@student.unisg.ch>
2025-05-13 17:54:35 +0200

    gg

--- app/app.py
+++ app/app.py
@@ -5,7 +5,6 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
-import streamlit as st
 import folium
 from streamlit_folium import st_folium
 import pandas as pd
