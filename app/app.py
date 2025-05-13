125d99208023744e80bde5fc8b430456f13f2f17
Rinor909 <rinor.abazi@student.unisg.ch>
2025-05-13 14:39:35 +0200

    gg

--- app/app.py
+++ app/app.py
@@ -123,7 +123,7 @@ def main():
             selected_baujahr = st.selectbox(
                 "",
                 options=list(range(1900, 2026, 5)),
-                index=22,  # Default Baujahr to 2010
+                index=25,  # Default Baujahr to 2010
                 format_func=lambda x: str(x),
             )
             # Transportation mode - Put this in its own container
